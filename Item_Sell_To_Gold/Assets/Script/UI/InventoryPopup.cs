9aa4db129890b2bebedf224d5e15672da231f269
Creat777 <mannam757@knou.ac.kr>
2025-02-13 15:37:19 +0900

    25.02.13

---
+++ Item_Sell_To_Gold/Assets/Script/UI/InventoryPopup.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class InventoryPopup : RefreshElement
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        element = UIElement.INVENTORY;
+        SetElementToManager();
+    }
+
+    protected override void SetElementToManager()
+    {
+        base.SetElementToManager();
+    }
+
+    override public void RefreshUI()
+    {
+        GameManager.Instance.RefreshInventory();
+    }
+
+    public void CloseBtn()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
